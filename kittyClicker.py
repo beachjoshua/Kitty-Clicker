c542373e2e1fb15172f1b25992029b70dedee615
beachjoshua <104524801+beachjoshua@users.noreply.github.com>
2023-12-20 21:17:07 -0600

    added upgrade class

--- kittyClicker.py
+++ kittyClicker.py
@@ -1,5 +1,43 @@
 import pygame
 
+class upgradeButton:
+    def __init__(self, name, price, amtPerSec, x, y, imagePath=None):
+        self.font=pygame.font.Font("PixelFont.ttf", 30)
+        self.price = price
+        self.amtPerSec = amtPerSec
+        self.x = x
+        self.y = y
+        self.name = name
+        self.text = self.font.render(str(f"{name} | Price:{self.price}"), True, "white")
+        self.textRect = self.text.get_rect(topright=(x,y))
+        
+    #change methods   
+    def changeXPos(self, x):
+        self.x = x
+        self.textRect = self.text.get_rect((self.x,self.y))
+        
+    def changeYPos(self, y):
+        self.y = y
+        self.textRect = self.text.get_rect((self.x,self.y))
+        
+    def updatePrice(self):
+        self.price = int(self.price*1.5)
+        self.text = self.font.render(str(f"{self.name} | Price:{self.price}"), True, "white")
+        self.textRect = self.text.get_rect(topright=(self.x, self.y))
+        
+    #getter methods
+    def getPrice(self):
+        return self.price
+    
+    def getAmtPerSec(self):
+        return self.amtPerSec
+    
+    def getTextRect(self):
+        return self.textRect
+    
+    def getText(self):
+        return self.text
+
 #CAT ANIMATION IF MOUSE HOVERS
 def catResizer(catRect, catResizeVal, catImage):
     mouseX, mouseY = pygame.mouse.get_pos()
@@ -41,6 +79,16 @@ if __name__ == "__main__":
     catPointRect.x = (width - catPointRect.width)//2
     catPointRect.y = (height - catPointRect.height)
     
+    #upgrades
+    buttonWidth, buttonHeight = 150, 50
+    upgradeX, upgradeY = width-25, 10
+    upgradeList = []
+    upgradeList.append(upgradeButton("Milk", 10, 1, upgradeX, upgradeY))
+    upgradeList.append(upgradeButton("Kibble", 100, 2, upgradeX, upgradeY+buttonHeight))
+    upgradeList.append(upgradeButton("Treats", 500, 4, upgradeX, upgradeY+buttonHeight*2))
+    upgradeList.append(upgradeButton("Catnip", 1000, 6, upgradeX, upgradeY+buttonHeight*3))
+    amtPerSec = 0
+    
     fps = 120
     framesElapsed = 0
     
@@ -67,10 +115,16 @@ if __name__ == "__main__":
                 mouseX, mouseY = pygame.mouse.get_pos()
                 if catRect.collidepoint(mouseX, mouseY):
                     catPointAmt += 1
+                for upgrade in upgradeList:
+                    if upgrade.getTextRect().collidepoint(mouseX, mouseY) and upgrade.getPrice()<=catPointAmt:
+                        catPointAmt-=upgrade.getPrice()
+                        amtPerSec += upgrade.getAmtPerSec()
+                        upgrade.updatePrice()
         
         
         if(framesElapsed==fps):
             framesElapsed=0
+            catPointAmt += amtPerSec
             
         
         screen.fill("tan")
@@ -85,6 +139,8 @@ if __name__ == "__main__":
         
         screen.blit(catPointText, catPointRect)
         screen.blit(resizedCat, catRect)
+        for upgrade in upgradeList:
+            screen.blit(upgrade.getText(), upgrade.getTextRect())
         
         framesElapsed+=1
         pygame.display.flip()
